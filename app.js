cce69c8e3f8ed52f67b57f66b18d2bf339adf9c5
Usman Khan <160907897+usmankhan0016@users.noreply.github.com>
2024-04-01 14:03:14 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+// // 11.Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+// let names: string[] = ["Ahsan", "Umar", "Haidar", "Talib"];
+// for (let i = 0; i < names.length; i++) {
+//   console.log(names[i]);
+// }
+// Exercise 12.
+let names = ["Ahsan", "Umar", "Haidar", "Talib"];
+let message = "Hey, what'sup ";
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ app.ts
@@ -0,0 +1,12 @@
+// // 11.Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+// let names: string[] = ["Ahsan", "Umar", "Haidar", "Talib"];
+// for (let i = 0; i < names.length; i++) {
+//   console.log(names[i]);
+// }
+
+// Exercise 12.
+let names: string[] = ["Ahsan", "Umar", "Haidar", "Talib"];
+let message:string= "Hey, what'sup ";
+for (let i = 0; i < names.length; i++) {
+  console.log(message+names[i]);
+}
